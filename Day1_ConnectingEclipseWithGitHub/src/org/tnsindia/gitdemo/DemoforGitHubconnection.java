f82e391b3248239ef91ee14e7c8059cee326feff
Hrishi <Hrishi@DESKTOP-C4SS7I4>
2022-11-17 12:02:41 +0530

    first commit

---
+++ Day1_ConnectingEclipseWithGitHub/src/org/tnsindia/gitdemo/DemoforGitHubconnection.java
@@ -0,0 +1,13 @@
+package org.tnsindia.gitdemo;
+
+//Demo on to connect eclipse with GitHub
+
+/*..l.Create a class and print a statment..*/
+public class DemoforGitHubconnection {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to TNS India Foundation");
+
+	}
+
+}
