a9f9a9a116161a1d2c77f7f09a3d18947f6ebadd
RUSHIKESH <RUSHIKESH@LAPTOP-7DRFOE2C>
2022-11-25 22:40:04 +0530

    First Commit

---
+++ Day-6_InheritanceAndBufferedReaderClass/src/org/tnsindia/inharitancedemo/Kitkat.java
@@ -0,0 +1,10 @@
+package org.tnsindia.inharitancedemo;
+//child class2
+public class Kitkat extends AndroidVersion{
+	
+	void print1()
+	{
+		System.out.println("version 4-kitkat");
+	}
+
+}
---
+++ Day-6_InheritanceAndBufferedReaderClass/src/org/tnsindia/inharitancedemo/Tiramisu.java
@@ -0,0 +1,12 @@
+package org.tnsindia.inharitancedemo;
+
+//child class1
+public class Tiramisu extends AndroidVersion {
+	
+	void print()
+	{
+		System.out.println("version 13-Tiramisu");
+	}
+	
+
+}
