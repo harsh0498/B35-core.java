588e0d20a306794cbdc7ccc3d3e1f062a7d1d310
RUSHIKESH <RUSHIKESH@LAPTOP-7DRFOE2C>
2022-11-28 19:11:57 +0530

    first commit

---
+++ Day-7_abstractAndInstanceOf/src/abstractdemo/ATMmachine.java
@@ -0,0 +1,17 @@
+package abstractdemo;
+//abstract class
+abstract class ATMMachine {
+	
+	//Abstract class contain abstract or non-abstract method
+	
+	//abstract method
+	abstract void withdraw();
+	
+	//Non-abstract method
+	void display()
+	{
+		System.out.println("Non-abstract method");
+	}
+	
+
+}
