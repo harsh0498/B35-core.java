821e29a1481b11c4d7dba274768f529fe75b4029
RUSHIKESH <RUSHIKESH@LAPTOP-7DRFOE2C>
2022-11-29 19:49:11 +0530

    first commit

---
+++ Day-8_finalAndSuperKeyword/src/finalkeyworddemo/FinalMethodWithParent.java
@@ -0,0 +1,14 @@
+package finalkeyworddemo;
+//parent class
+public class FinalMethodWithParent {
+	
+	public long aadhar_no;
+	
+	//final keyword with method
+	public final void print()
+	{
+		System.out.println("Aadhar no.is: "+aadhar_no);
+	}
+	
+
+}
