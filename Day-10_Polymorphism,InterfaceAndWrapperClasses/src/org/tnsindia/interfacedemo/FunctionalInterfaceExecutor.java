0b3e3cda9236dd9ef66e330d2e83110e54df35c5
RUSHIKESH <RUSHIKESH@LAPTOP-7DRFOE2C>
2022-12-02 19:47:50 +0530

    first commit

---
+++ Day-10_Polymorphism,InterfaceAndWrapperClasses/src/org/tnsindia/interfacedemo/FunctionalInterfaceExecutor.java
@@ -0,0 +1,10 @@
+package org.tnsindia.interfacedemo;
+//driver class
+public class FunctionalInterfaceExecutor {
+
+	public static void main(String[] args) {
+		ImplementableFunctionalInterfaceClass e=new ImplementableFunctionalInterfaceClass();
+		e.print("Thanks Mam, for Teaching us");
+	}
+
+}
